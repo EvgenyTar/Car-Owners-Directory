fa81e219d880aa14adaf853d1d9de79391ffa088
evgen <evgenytar@gmail.com>
2021-04-10 11:58:48 +0300

    Добавление и удаление авто у владельцев на сервере

--- src/app/service/car-owners.service.ts
+++ src/app/service/car-owners.service.ts
@@ -1,7 +1,7 @@
 import { CarEntity } from './../model/car';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { map, switchMap, tap } from 'rxjs/operators';
+import { concat, Observable, of } from 'rxjs';
+import { map, switchMap, take, tap } from 'rxjs/operators';
 import { TestData } from '../data/TestData';
 import ICarOwnersService from '../interface/interfaces';
 import { OwnerEntity } from '../model/owner';
@@ -52,15 +52,6 @@ export class CarOwnersService implements ICarOwnersService {
         );
       })
     );
-    // return of(
-    //   TestData.owners.map((item) => {
-    //     const idOwner = item.id;
-    //     const cars = this.getCarsByOwnerId(idOwner);
-    //     const owner = OwnerEntity.createFromAny(item);
-    //     owner.cars = cars;
-    //     return owner;
-    //   })
-    // );
   }
 
   getOwnerById(aId: number): Observable<OwnerEntity> {
@@ -72,18 +63,6 @@ export class CarOwnersService implements ICarOwnersService {
         return new OwnerEntity(-1, '', '', '');
       })
     );
-    // return of(aId).pipe(
-    //   map((id) => {
-    //     const findOwner = TestData.owners.find((item) => item.id === id);
-    //     if (!findOwner) {
-    //       return new OwnerEntity(-1, '', '', '');
-    //     }
-    //     const cars = this.getCarsByOwnerId(id);
-    //     const owner = OwnerEntity.createFromAny(findOwner);
-    //     owner.cars = cars;
-    //     return owner;
-    //   })
-    // );
   }
 
   createOwner(
@@ -101,32 +80,13 @@ export class CarOwnersService implements ICarOwnersService {
       .pipe(
         map((owner) => {
           return OwnerEntity.createFromAny(owner);
+        }),
+        switchMap((owner) => {
+          return this._createCars(owner.id, aCars).pipe(
+            switchMap((_) => this.getOwnerById(owner.id))
+          );
         })
       );
-    // return of(true).pipe(
-    //   switchMap((_) => {
-    //     const newId =
-    //       TestData.owners
-    //         .map((item) => item.id)
-    //         .reduce((max, val) => (max > val ? max : val)) + 1;
-
-    //     TestData.owners.push({
-    //       id: newId,
-    //       firstName: aFirstName,
-    //       lastName: aLastName,
-    //       middleName: aMiddleName,
-    //     });
-
-    //     let maxCarId = TestData.getMaxIdCar() + 1;
-
-    //     for (let i = 0; i < aCars.length; i++) {
-    //       aCars[i].idOwner = newId;
-    //       aCars[i].id = maxCarId++;
-    //       TestData.cars.push(aCars[i]);
-    //     }
-    //     return this.getOwnerById(newId);
-    //   })
-    // );
   }
 
   editOwner(aOwner: OwnerEntity): Observable<OwnerEntity> {
@@ -135,60 +95,36 @@ export class CarOwnersService implements ICarOwnersService {
         return this.getOwnerById(aOwner.id);
       })
     );
-    // return of(true).pipe(
-    //   map((_) => {
-    //     let findOwner = TestData.owners.find((item) => item.id === aOwner.id);
-    //     if (!findOwner) {
-    //       return new OwnerEntity(-1, '', '', '');
-    //     }
-    //     let maxCarId = 0;
-
-    //     if (TestData.owners.find((item) => item.id === -1)) {
-    //       maxCarId = TestData.getMaxIdCar() + 1;
-    //     }
-    //     findOwner.firstName = aOwner.firstName;
-    //     findOwner.lastName = aOwner.lastName;
-    //     findOwner.middleName = aOwner.middleName;
-
-    //     for (let i = 0; i < aOwner.cars.length; i++) {
-    //       let findCarEntity = TestData.cars.find(
-    //         (item) => item.id === aOwner.cars[i].id
-    //       );
-    //       if (findCarEntity) {
-    //         findCarEntity.carManufacturer = aOwner.cars[i].carManufacturer;
-    //         findCarEntity.carModel = aOwner.cars[i].carModel;
-    //         findCarEntity.productionYear = aOwner.cars[i].productionYear;
-    //         findCarEntity.registrationMark = aOwner.cars[i].registrationMark;
-    //       } else {
-    //         if (aOwner.cars[i].id === -1) {
-    //           aOwner.cars[i].id = maxCarId++;
-    //           TestData.cars.push(aOwner.cars[i]);
-    //         }
-    //       }
-    //     }
-    //     return aOwner;
-    //   })
-    // );
   }
 
   deleteOwner(aOwnerId: number): Observable<void> {
     let ownersUrl = this.ownersUrl + '/' + aOwnerId;
 
-    return this.httpClient.delete<void>(ownersUrl);
+    let cars = this.getCarsByOwnerId(aOwnerId);
+    let carsId = cars.map((car) => car.id);
 
-    // for (let i = 0; i < TestData.cars.length; i++) {
-    //   if (TestData.cars[i].idOwner === aOwnerId) {
-    //     TestData.cars.splice(i, 1);
-    //     i--;
-    //   }
-    // }
+    return this.httpClient
+      .delete<void>(ownersUrl)
+      .pipe(switchMap((_) => this._deleteCars(carsId)));
+  }
 
-    // const ownerIndex = TestData.owners.findIndex(
-    //   (item) => item.id === aOwnerId
-    // );
-    // if (ownerIndex !== -1) {
-    //   TestData.owners.splice(ownerIndex, 1);
-    // }
+  private _createCars(idOwner: number, cars: CarEntity[]): Observable<any> {
+    for (const car of cars) {
+      car.idOwner = idOwner;
+    }
+    return concat(
+      ...cars.map((item) => {
+        return this.httpClient.post(this.carsUrl, item);
+      })
+    );
+  }
+
+  private _deleteCars(carsId: number[]): Observable<any> {
+    return concat(
+      ...carsId.map((id) => {
+        return this.httpClient.delete(this.carsUrl + '/' + id);
+      })
+    );
   }
 
   getCarsByOwnerId(ownerId: number): CarEntity[] {
